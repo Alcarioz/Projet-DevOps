585fe9d4169a8f8c237dc4d2a847fae8d9f022e3
Benjamin Défossé <Benj.Defosse@gmail.com>
2018-04-04 11:11:17 +0200

    Mise en place des tests

---
+++ src/test/java/lesPetitsPedestres/DataframeTest.java
@@ -0,0 +1,19 @@
+package lesPetitsPedestres;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class DataframeTest extends TestCase {
+	
+	public DataframeTest(String testName){		
+		super(testName);
+	}
+	
+	public static Test suite(){
+		return new TestSuite( DataframeTest.class );
+	}
+	
+	
+
+}
---
+++ src/test/java/lesPetitsPedestres/SeriesTest.java
@@ -0,0 +1,46 @@
+package lesPetitsPedestres;
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class SeriesTest extends TestCase {
+
+	
+	public SeriesTest(String testName){		
+		super(testName);
+	}
+	
+	public static Test suite(){
+		return new TestSuite( SeriesTest.class );
+	}
+	
+
+	public void testgetLabel(){
+		
+		
+	}
+	
+	public void testgetArray(){
+		
+		
+	}
+	
+	public void testgetSize(){
+		
+		
+	}
+	
+	public void testgetaddVal(){
+		
+		
+	}
+	
+	public void testgetVal(){
+		
+		
+	}
+	
+
+}
