82c1d3a585d0083f7a2263600ffad7517f1f8d4a
Benjamin Défossé <Benj.Defosse@gmail.com>
2018-04-11 09:14:36 +0200

    Mise en place tests

--- src/test/java/lesPetitsPedestres/SeriesTest.java
+++ src/test/java/lesPetitsPedestres/SeriesTest.java
@@ -27,6 +27,13 @@ public class SeriesTest extends TestCase {
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void testgetLabel2(){
+		ArrayList<Boolean> list = new ArrayList<Boolean>();
+		Series<Boolean> test = new Series("Test2",list);
+		assert(test.getLabel()=="Test2");
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void testgetArray(){
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(5);
@@ -38,9 +45,34 @@ public class SeriesTest extends TestCase {
 		assert(test.getArray().equals(list));
 	}
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void testgetArray2(){
+		ArrayList<Character> list = new ArrayList<Character>();
+		list.add('c');
+		list.add('r');
+		list.add('g');
+		list.add('z');
+		Series<Character> test = new Series("Test",list);
+		assert(test.getArray().equals(list));
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void testgetSize(){
-		
-		assert(true);
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Bonjour");
+		list.add("Je fais des tests");
+		list.add("Tu vas bien?");
+		list.add("J'espère que tu vas bien");
+		Series<String> test = new Series("Test",list);
+		list.add("Oui");
+		assert(test.getSize()==list.size());
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void testgetSize2(){
+		ArrayList<String> list = new ArrayList<String>();
+		Series<String> test = new Series("Test",list);
+		assert(test.getSize()==list.size());
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -48,12 +80,18 @@ public class SeriesTest extends TestCase {
 		ArrayList<String> list = new ArrayList<String>();	
 		Series<String> test = new Series("Test",list);
 		test.addVal("test JUnit");
-		assert(true);
+		assert(list.get(0)=="test Junit");
 	}
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void testgetVal(){
-		
-		assert(true);
+		ArrayList<Boolean> list = new ArrayList<Boolean>();
+		list.add(true);
+		list.add(false);
+		list.add(true);
+		list.add(true);
+		Series<Boolean> test = new Series("Test",list);
+		assert(test.getVal(2)==false);
 	}
 	
 
