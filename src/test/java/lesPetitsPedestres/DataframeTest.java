18275e16afa36291cef1012a4b9cb5ad8ecf8ad8
Benjamin Défossé <Benj.Defosse@gmail.com>
2018-04-11 14:52:20 +0200

    Test Dataframe

--- src/test/java/lesPetitsPedestres/DataframeTest.java
+++ src/test/java/lesPetitsPedestres/DataframeTest.java
@@ -53,39 +53,6 @@ public class DataframeTest extends TestCase {
 		assertTrue(outPut.contains("2 48 Non"));
 	}
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void testprintAll2(){
-		ArrayList<Integer> list1 = new ArrayList<Integer>();
-		list1.add(5);
-		list1.add(9);
-		list1.add(48);
-		
-		ArrayList<String> list2 = new ArrayList<String>();
-		list2.add("Bonjour");
-		list2.add("Oui");
-		Series<Integer> serie1 = new Series("Serie1",list1);
-		Series<String> serie2 = new Series("Serie2",list2);
-		ArrayList<Series> series = new ArrayList<Series>();
-		series.add(serie1);
-		series.add(serie2);
-		Dataframe test = new Dataframe(series);
-		
-		PrintStream oldOut = System.out;
-		ByteArrayOutputStream newOut = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(newOut));
-		
-		test.printAll();
-		
-		System.setOut(oldOut);
-		
-		String outPut = new String(newOut.toByteArray());
-		
-		assertTrue(outPut.contains("  Serie1 Serie2"));
-		assertTrue(outPut.contains("0 5 Bonjour"));
-		assertTrue(outPut.contains("1 9 Oui"));
-		assertTrue(outPut.contains("2 48"+null));
-	}
-	
 	public void testprintAllFile() throws IOException{
 		Dataframe test = new Dataframe("Test.csv");
 		
