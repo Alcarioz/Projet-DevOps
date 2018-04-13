6cea3e75abf4b1565e4590452c68a8a67bfb120e
Benjamin Défossé <Benj.Defosse@gmail.com>
2018-04-13 16:13:12 +0200

    Code Final Du Projet Devops

--- src/test/java/lesPetitsPedestres/DataframeTest.java
+++ src/test/java/lesPetitsPedestres/DataframeTest.java
@@ -301,8 +301,8 @@ public class DataframeTest extends TestCase {
 		String outPut = new String(newOut.toByteArray());
 		
 		assertTrue(outPut.contains("  Sexe Prénom Année de naissance Langage Marque"));
-		assertTrue(outPut.contains("1 F Béatrice 1984 Java HP"));
-		assertTrue(outPut.contains("3 M Jeremy 1979 Java ACER"));
+		assertTrue(outPut.contains("0 F Béatrice 1984 Java HP"));
+		assertTrue(outPut.contains("1 M Jeremy 1979 Java ACER"));
 	}
 
 }
